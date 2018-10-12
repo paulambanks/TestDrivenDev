4ed1e257564be09d2450f41cb9a524076afaded3
paulambanks <bpaula92@gmail.com>
2018-10-12 15:11:17 +0100

    Functional test now checks I can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -17,17 +19,32 @@ class NewVisitorTest(unittest.TestCase):
 
         # I should notice the page title and header mention to-do list
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # I should be invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # A text box should allow me to type a task
+        inputbox.send_keys('Buy a display cable for the PC')
+
+        # When I hit Enter, the page should update and I should see my new item
+        # '1: Buy a display cable for the PC' listed on the page of to-do list table.
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy a display cable for the PC' for row in rows)
+        )
+
+        # A text box to add another item should be still visible. I should be able to add another item
         self.fail('Finish the test!')
-
-    # I should be invited to enter a to-do item straight away
-
-    # A text box should allow me to type a task
-
-    # When I hit Enter, the page should update , and I should see my new item listed on the page of to-do list.
-
-    # A text box to add another item should be still visible.
-
-    # I should be able to add another item
 
     # On Enter I should see both items on to-do page.
 
