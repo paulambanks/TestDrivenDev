40d7dde7dada92639f4ed54f75c141434e6cebf8
paulambanks <bpaula92@gmail.com>
2018-10-25 12:29:08 +0100

    Customise the validation error for suplicate items, adjust the error message

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -3,7 +3,7 @@ from lists.models import Item, List
 from django.utils.html import escape
 from lists.forms import ItemForm
 from lists.forms import EMPTY_ITEM_ERROR
-
+from unittest import skip
 
 class HomePageTest(TestCase):
 
@@ -93,6 +93,20 @@ class ListViewTest(TestCase):
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
+    @skip
+    def test_duplicate_item_validation_error_end_up_on_list_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            f'/lists/{list1.id}/',
+            data={'text': 'textey'}
+        )
+
+        expected_error = escape("You've already got this in your list")
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
 
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
