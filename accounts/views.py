3f07b4b1938f7d51eff3cb781b88ac03fa90e711
paulambanks <bpaula92@gmail.com>
2018-10-29 12:44:00 +0000

    Custom passwordless auth backend and custom user model

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
+import sys
+
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail
-from django.contrib import messages
+from django.contrib import messages, auth
+
 from django.urls import reverse
 from accounts.models import Token
 
@@ -26,4 +29,7 @@ def send_login_email(request):
 
 
 def login(request):
+    user = auth.authenticate(uid=request.GET.get('token'))
+    if user:
+        auth.login(request, user)
     return redirect('/')
